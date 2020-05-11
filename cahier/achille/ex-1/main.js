86f9b433395f0485cc8759945de70d4c326f23cc
hille22 <47397572+hille22@users.noreply.github.com>
2020-05-11 11:21:15 +0200

    draw pixel circle

--- cahier/achille/ex-1/main.js
+++ cahier/achille/ex-1/main.js
@@ -11,12 +11,20 @@ function setup(){
 	createCanvas(windowWidth, windowHeight);
 }
 
+function sign(v){
+	if(v < 0) return -1
+	else return 1
+}
+
 function draw(){
 
 	for(let j=0; j<NUM_Y; j++) {
 		for(let i=0; i<NUM_X; i++){
 			const idx = i + j * NUM_X;
-			data[idx] = (2*3.14)*16;
+			const u = (i * 2 - NUM_X) / NUM_X;
+			const v = (j * 2 - NUM_Y) / NUM_Y;
+			let d = dist(0,0,u,v) -1.0
+			data[idx] =  sign(d);
 		}
 	}
 	//translate(windowWidth/2-(i * CELL), windowHeight/2-(y * CELL))
@@ -24,6 +32,9 @@ function draw(){
 
 	const ox = (width-(NUM_X) * CELL) /2
 	const oy = (height-(NUM_Y) * CELL) /2
+
+	textSize(9);
+	textAlign(CENTER,CENTER)
 
 	for(let j=0; j<NUM_Y; j++) {
 		for(let i=0; i<NUM_X; i++){
@@ -33,6 +44,8 @@ function draw(){
 			const v = data[idx];
 			fill(v * 255);
 			rect(x,y,CELL,CELL);
+			// fill(255,0,0)
+			// text(v,x+CELL/2,y+CELL/2)
 		}
 	}
 
